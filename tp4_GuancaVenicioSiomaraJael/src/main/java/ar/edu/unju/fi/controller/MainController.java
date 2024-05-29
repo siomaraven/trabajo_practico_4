91cf4f18ca6e8338961e1cf1b6d21f5de1629dc2
parad <parad@Ryzen>
2024-05-29 00:09:34 -0300

    Primer commit del index en la rama Bootstrap

--- tp4_GuancaVenicioSiomaraJael/src/main/java/ar/edu/unju/fi/controller/MainController.java
+++ tp4_GuancaVenicioSiomaraJael/src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -1,10 +1,15 @@
 package ar.edu.unju.fi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/blue")
 public class MainController {
-
+	
+	@GetMapping("/index")
+	public String getIndexpage(){
+		return "index";
+	}
 }
